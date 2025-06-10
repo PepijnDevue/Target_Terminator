51db1a979f456274032340ba2bae4b4e23d4a8a1
Pepijn <pepijndevue@hotmail.com>
2025-06-10 16:55:52 +0200

    Beautiful urinal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 """Main module for running the Target Terminator environment."""
 import random
-import time
 
 from __init__ import make
 
@@ -11,9 +10,7 @@ env = make(
     target_config="config/fly.yaml",
 )
 
-start_time = time.time()
-
-for frames in range(100_000):
+for _ in range(100_000):
     # in keyboard mode the random.choice will be ignored
     state, reward, terminated, truncated, _ = env.step(random.choice([1,2,3,4,5]))
     
@@ -21,6 +18,4 @@ for frames in range(100_000):
     if terminated or truncated:
         env.reset()
 
-    if frames % 100 == 0:
-        print(f"FPS: {frames / (time.time() - start_time):.2f}")
 env.close()
